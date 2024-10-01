9d4b9419ca889cbf59de939527eccee46a1feed2
Amato Luca <amatoroccoluca@gmail.com>
2024-10-01 16:58:07 +0200

    chore: add new task component

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,16 +1,27 @@
 import React from 'react';
 import NewTask from './NewTask';
 
-const Tasks = () => {
+const Tasks = ({ tasks, onAdd, onDelete }) => {
   return (
     <section>
       <h2 className="text-2xl font-bold text-stone-700 mb-4">Tasks</h2>
-      New Tasks
-      <NewTask />
-      <p className="text-stone-800 my-4">
-        This project does not have any tasks yet.
-      </p>
-      <ul></ul>
+      <NewTask onAdd={onAdd} />
+      {tasks.length === 0 ? (
+        <p className="text-stone-800 my-4">
+          This project does not have any tasks yet.
+        </p>
+      ) : (
+        <ul>
+          {tasks.map((task) => (
+            <li key={task.id} className="flex justify-between my-4">
+              <span>{task.text}</span>
+              <button className="text-stone-700 hover:text-red-500">
+                clear
+              </button>
+            </li>
+          ))}
+        </ul>
+      )}
     </section>
   );
 };
